647888bb5b33a187014e6ee2e43f84ca6df57c87
Roar <rfo909@hotmail.com>
2024-01-01 11:59:17 +0100

    x

--- src/rf2/rf2.c
+++ src/rf2/rf2.c
@@ -12,7 +12,7 @@ int main()
     // dropped characters solved by implementing circular buffer in Serial.c
     // TIB* has separate circular buffer for TIB* pointer logic
     // The TIB functions seem to work
-    // Sym.c seems to work
+    // Sym.c seems to work + the initial setup from ACode.txt also ok (eq, ne, and etc)
 
     for(;;) {
 
@@ -31,10 +31,9 @@ int main()
         Ref symbolRef = symCreateTIBWord();
         sprintf(buf,"symbolRef=%d\r\n", symbolRef);
         serialEmitStr(buf);
-        //char *s = (char *) refToPointer(symbolRef);
-        //sprintf(buf,"%d ", symbolRef);
-        //serialEmitStr(buf);
-        //serialEmitStr(s);
+        char *s = (char *) refToPointer(symbolRef);
+        DEBUG("symbolRef to pointer: ");
+        serialEmitStr(s);
         serialEmitNewline();
         
         confirmTIBr();
