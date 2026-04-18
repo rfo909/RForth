5263714195897d7da3b246f771d48e094e0e5e54
Roar Foshaug <rfo909@hotmail.com>
2026-04-18 19:22:20 +0200

    x

--- Arduino/RForth/RForth.ino
+++ Arduino/RForth/RForth.ino
@@ -43,90 +43,108 @@ void setup() {
 
 }
 
-// -----------------------------
-// Deep sleep stuff
-// -----------------------------
+// ##########################################
+// atmega328p specific code
 
-ISR(WDT_vect) {
-  cli();
-  disableWatchdogInterrupt();
-  sei();
-}
+#if defined(__AVR_ATmega328P__)
+  // -----------------------------
+  // Deep sleep stuff
+  // -----------------------------
 
-void disableWatchdogInterrupt() {
-  WDTCSR &= ~(1<<WDIE);
-}
-
-void enableADC() {
-  ADCSRA |= (1<<7);
-}
-
-void doDeepSleep() {
-
-  // store data direction registers for pins
-  Byte ddrb=DDRB;
-  Byte ddrc=DDRC;
-  Byte ddrd=DDRD;
-
-  Byte portb=PORTB;
-  Byte portc=PORTC;
-  Byte portd=PORTD;
-
-  for (int i=0; i<20; i++) {
-    pinMode(i,OUTPUT);  // saves milliamps
-    digitalWrite(i,0);
+  ISR(WDT_vect) {
+    cli();
+    disableWatchdogInterrupt();
+    sei();
   }
 
-  //blip(2);
-  // https://www.youtube.com/watch?v=urLSDi7SD8M
 
-  // disable ADC
-  ADCSRA &= ~(1<<7);
+  void disableWatchdogInterrupt() {
+    WDTCSR &= ~(1<<WDIE);
+  }
 
-  cli();
+  void enableADC() {
+    ADCSRA |= (1<<7);
+  }
 
-  // Setup watchdog timer to wake from sleep
-  // 7     6   5    4    3   2    1    0
-  // WDIF WDIE WDP3 WDCE WDE WDP1 WDP1 WDP0
-  // WDP* are prescaler bits, note WDP3 separate from the others
+  void doDeepSleep() {
 
-  WDTCSR = (1<<WDCE) | (1<<WDE) ; // b00011000;  // (24); // change enable and WDE - also resets
+    // store data direction registers for pins
+    Byte ddrb=DDRB;
+    Byte ddrc=DDRC;
+    Byte ddrd=DDRD;
+
+    Byte portb=PORTB;
+    Byte portc=PORTC;
+    Byte portd=PORTD;
+
+    for (int i=0; i<20; i++) {
+      pinMode(i,OUTPUT);  // saves milliamps
+      digitalWrite(i,0);
+    }
+
+    //blip(2);
+    // https://www.youtube.com/watch?v=urLSDi7SD8M
+
+    // disable ADC
+    ADCSRA &= ~(1<<7);
+
+    cli();
+
+    // Setup watchdog timer to wake from sleep
+    // 7     6   5    4    3   2    1    0
+    // WDIF WDIE WDP3 WDCE WDE WDP1 WDP1 WDP0
+    // WDP* are prescaler bits, note WDP3 separate from the others
+
+    WDTCSR = (1<<WDCE) | (1<<WDE) ; // b00011000;  // (24); // change enable and WDE - also resets
 
 
-  WDTCSR = (1<<WDP3) | (1<<WDP0) ; // b00100001;  // (33); // prescalers only, get rid of WDCE and WDE - 8 seconds
-  //WDTCSR = (1<<WDP2) | (1<<WDP1) | (1<<WDP0) ; // 2 seconds
-  //WDTCSR = (1<<WDP3) ; // 4 seconds
+    WDTCSR = (1<<WDP3) | (1<<WDP0) ; // b00100001;  // (33); // prescalers only, get rid of WDCE and WDE - 8 seconds
+    //WDTCSR = (1<<WDP2) | (1<<WDP1) | (1<<WDP0) ; // 2 seconds
+    //WDTCSR = (1<<WDP3) ; // 4 seconds
+    
+    WDTCSR |= (1<<WDIE)  ; // (1<<6); // enable interrupt mode: WDE=0, WDIE=1 (but also requires FUSE WDTON=1)
+    //WDTCSR |= (1<<WDE) | (1<<WDIE)  ; // interrupt and system reset
+    
+      // WDE | WDIE = interrupt and system reset
+
+      // Even when only settng WDIE we get system reset. Do I need to create an interrupt handler
   
-  WDTCSR |= (1<<WDIE)  ; // (1<<6); // enable interrupt mode: WDE=0, WDIE=1 (but also requires FUSE WDTON=1)
-  //WDTCSR |= (1<<WDE) | (1<<WDIE)  ; // interrupt and system reset
+    sei();
   
-    // WDE | WDIE = interrupt and system reset
+    // enable sleep
+    SMCR |= (1<<2); // power down mode
+    SMCR |= 1; // enable sleep
 
-    // Even when only settng WDIE we get system reset. Do I need to create an interrupt handler
- 
-  sei();
- 
-  // enable sleep
-  SMCR |= (1<<2); // power down mode
-  SMCR |= 1; // enable sleep
+    
+    // BOD disable - must go to sleep within 4 clock cycles, so inline here
+    MCUCR |= (3<<5);  // set boths BODS and BODSE at the same time
+    MCUCR = (MCUCR & ~(1<<5)) | (1<<6); // set BODS and clear BODSE
+    __asm__ __volatile__("sleep");
 
-  
-  // BOD disable - must go to sleep within 4 clock cycles, so inline here
-  MCUCR |= (3<<5);  // set boths BODS and BODSE at the same time
-  MCUCR = (MCUCR & ~(1<<5)) | (1<<6); // set BODS and clear BODSE
-  __asm__ __volatile__("sleep");
+    // back from sleep
+    DDRB=ddrb;
+    DDRC=ddrc;
+    DDRD=ddrd;
+    PORTB=portb;
+    PORTC=portc;
+    PORTD=portd;
+    disableWatchdogInterrupt();
+    enableADC();
 
-  // back from sleep
-  DDRB=ddrb;
-  DDRC=ddrc;
-  DDRD=ddrd;
-  PORTB=portb;
-  PORTC=portc;
-  PORTD=portd;
-  disableWatchdogInterrupt();
-  enableADC();
+  }
 
-}
+#else
+  // #############################
+  // Not compiling for atmega328p: dummy functions
+
+  void disableWatchdogInterrupt() {
+  }
+
+  void doDeepSleep() {
+    delay(8000);
+  }
+
+#endif
 
 // -------------------------------------------
 // Serial output (public)
